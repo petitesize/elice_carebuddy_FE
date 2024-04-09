c469f0627ad02048cf78472bdcd365fa18b6080e
k65860 <jy_0327@likelion.org>
2024-04-10 01:29:32 +0900

    Feat: BigModal Component Add

--- src/components/baseComponent/BigModal.tsx
+++ src/components/baseComponent/BigModal.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import styled from 'styled-components';
+import Button from '../../components/baseComponent/Button';
 
 const ModalBackground = styled.div`
   position: fixed;
@@ -21,14 +22,26 @@ const ModalContent = styled.div`
   background-color: var(--color-white);
   border-radius: 7px;
   border: 1px solid var(--color-grey2);;
-  padding: 30px;
+  border-top: 20px solid #6d987a;
+  padding: 20px 30px;
   display: flex;
   z-index: 10000;
 `;
 
 const Container = styled.div`
-  display: flex;
   padding: 10px;
+`;
+
+const Title = styled.div`
+  font-weight: var(--font-weight-bold);
+  font-size: var(--font-size-hd-1); //20
+  color: var(--color-green-main);
+  padding-bottom: 10px;
+  border-bottom: 1px solid var(--color-grey-2);
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
 `;
 
 interface ModalProps {
@@ -45,7 +58,12 @@ const RecordModal: React.FC<ModalProps> = ({ onClose, component: Component }) =>
     <ModalBackground onClick={handleModalBackgroundClick}>
       <ModalContent onClick={(e) => e.stopPropagation()}>
         <Container>
+          <Title>병원기록</Title>
           {Component}
+          <ButtonContainer>
+            <Button variant="primary" fontSize="ft-1" padding="0 20px" margin="0 10px 0 0">등록</Button>
+            <Button fontSize="ft-1" padding="0 20px" onClick={handleModalBackgroundClick}>취소</Button>
+          </ButtonContainer>
         </Container>
       </ModalContent>
     </ModalBackground>
