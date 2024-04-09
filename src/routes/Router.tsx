fce430476f500dc99887247ddd226ab23afc922e
chacha <yushin.pak@gmail.com>
2024-04-10 03:00:50 +0900

    Feat: partially completed Writing Modal

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -11,8 +11,8 @@ import Diary from '../pages/diary/Diary';
 import HospitalInfo from '../pages/hospitalInfo/HospitalInfo';
 import PharmacyInfo from '../pages/pharmacyInfo/PharmacyInfo';
 import PetRegister from '../pages/petRegister/PetRegister';
-import PetEdit from '../pages/petEdit/PetEdit'
-import WritingForModal from '../components/community/WritingForModal'; //테스트용 - 추후 삭제 예정
+import PetEdit from '../pages/petEdit/PetEdit';
+import WritingForModal from '../components/community/WritingModal'; //테스트용 - 추후 삭제 예정
 import NotFound from '../pages/notFound/NotFound';
 
 const AppRouter = () => {
@@ -25,15 +25,27 @@ const AppRouter = () => {
         <Route path="/userpage" element={<Layout component={Userpage} />} />
         <Route path="/" element={<Layout component={Home} />} />
         <Route path="/community" element={<Layout component={Community} />} />
-        <Route path="/pet-register" element={<Layout component={PetRegister} />} />
+        <Route
+          path="/pet-register"
+          element={<Layout component={PetRegister} />}
+        />
         <Route path="/pet-edit" element={<Layout component={PetEdit} />} />
         <Route path="/post" element={<Layout component={Post} />} />
         <Route path="/group" element={<Layout component={Group} />} />
         <Route path="/diary" element={<Layout component={Diary} />} />
-        <Route path="/hospital-info" element={<Layout component={HospitalInfo} />} />
-        <Route path="/pharmacy-info" element={<Layout component={PharmacyInfo} />} />
-        <Route path="/writing-for-modal" element={<Layout component={WritingForModal} />} /> {/* 테스트용 - 추후 삭제 예정 */}
-        
+        <Route
+          path="/hospital-info"
+          element={<Layout component={HospitalInfo} />}
+        />
+        <Route
+          path="/pharmacy-info"
+          element={<Layout component={PharmacyInfo} />}
+        />
+        <Route
+          path="/writing-for-modal"
+          element={<Layout component={WritingForModal} />}
+        />{' '}
+        {/* 테스트용 - 추후 삭제 예정 */}
         {/* 모든 경로에 대한 매칭을 시도한 후 NotFound 컴포넌트를 렌더링합니다. */}
         <Route path="*" element={<NotFound />} />
       </Routes>
