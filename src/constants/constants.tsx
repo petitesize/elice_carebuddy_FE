9bbb6eca46862613d78250adab4bf80a5b2c4e86
k65860 <jy_0327@likelion.org>
2024-04-16 11:01:06 +0900

    Fix: search error

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,5 +1,5 @@
 // 상수 파일
-// export const API_URL = 'http://localhost:3001/api/';
-export const API_URL = 'http://kdt-sw-8-team01.elicecoding.com/api/';
+export const API_URL = 'http://localhost:3001/api/';
+//export const API_URL = 'http://kdt-sw-8-team01.elicecoding.com/api/';
 
 export const DOMAIN_URL = 'http://kdt-sw-8-team01.elicecoding.com/';
