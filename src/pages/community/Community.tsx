b2e962fda8dffdf6fe2b89a2a5fe0f181399d671
chacha <yushin.pak@gmail.com>
2024-04-18 07:04:54 +0900

    Completed almost All for community part

--- src/pages/community/Community.tsx
+++ src/pages/community/Community.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import { API_URL, DOMAIN_URL } from '../../constants/constants';
+import {
+  API_URL,
+  DOMAIN_URL,
+  UPLOADED_IMG_URL,
+} from '../../constants/constants';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { userState } from '../../recoil/atoms';
+import GetRandomItems from '../../utils/GetRandomThreeItems'; // GetRandomItems 함수 import
 
 // 컴포넌트
 import Search from '../../components/baseComponent/Search';
@@ -14,30 +19,11 @@ import MemberListSidebar from '../../components/community/MemberListSidebar';
 import LinkButton from '../../components/baseComponent/LinkButton';
 import WritingModalButton from '../../components/community/WritingModalButton';
 
-// 임시 이미지
-const memberArray = [
-  <MemberListSidebar
-    src={profileImg}
-    nickname={tempNickname}
-    introduction={tempIntroduction}
-  />,
-  <MemberListSidebar
-    src={profileImg}
-    nickname={tempNickname}
-    introduction={tempIntroduction}
-  />,
-  <MemberListSidebar
-    src={profileImg}
-    nickname={tempNickname}
-    introduction={tempIntroduction}
-  />,
-];
+// 로직
+import sortByLatest from '../../utils/sortByLatest';
 
-import {
-  profileImg,
-  tempNickname,
-  tempIntroduction,
-} from '../../../temp-data-community';
+// 임시 이미지
+import { profileImg } from '../../../temp-data-community';
 
 interface Post {
   title: string;
@@ -52,31 +38,69 @@ interface Post {
   };
 }
 
-interface Member {}
+interface Member {
+  profileImage: string[];
+  nickName: string;
+  introduce: string;
+}
 
 const Community: React.FC = () => {
-  const { groupId } = useParams<{ groupId: string }>();
   const navigate = useNavigate();
   const [user] = useRecoilState(userState);
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [members, setMembers] = useState<Member[]>([]);
+  const { groupId } = useParams<{ groupId: string }>();
+  const [posts, setPosts] = useState<Post[]>([]); // 그룹의 전체 게시글
+  const [groupMembers, setGroupMembers] = useState<Member[]>([]); // 현재 그룹에 소속된 멤버
+  const [memberArray, setMemberArray] = useState<JSX.Element[]>([]); // 추천 그룹 멤버
   const [filteredSearchPosts, setFilteredSearchPosts] = useState<Post[]>([]); // 검색 결과 저장
-  const [searchedPostCount, setSearchedPostCount] = useState(0);
+  const [searchedPostCount, setSearchedPostCount] = useState(0); // 검색된 게시글 수
+  const [searchValue, setSearchValue] = useState<string | null>(null); // 검색어
 
-  // 추천 멤버 조회 API -> 커뮤니티 추천 멤버로 로직 변경 필요
+  // 커뮤니티 멤버 조회 API
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // 전체 유저 조회
         const response = await axios.get(`${API_URL}users`);
-        setMembers(response.data.message);
-        console.log('멤버 조회 성공', response.data.message);
+        const allMembers: Member[] = response.data.message; 
+        console.log('posts', posts);
+
+        // 현재 그룹에 속한 멤버만 선택
+        const groupMembers = allMembers.filter((member) =>
+          member.categoryId.includes(posts[0]?.categoryId?._id),
+        );
+
+        // 소속 멤버에서 '유저'를 제외하기(유저 추천을 위한 멤버 조회이기 때문)
+        const currentUser = groupMembers.find((member) => member._id === user?._id);
+        const filteredGroupMembers = groupMembers.filter((member) => member._id !== currentUser?._id);        
+
+        setGroupMembers(filteredGroupMembers);
+        console.log('멤버 조회 성공', filteredGroupMembers);
       } catch (error) {
         console.error('멤버 조회 실패', error);
       }
     };
 
     fetchData();
+  }, [posts]);
+
+  // 멤버 관련 로직
+  useEffect(() => {
+    // 추천 그룹 뽑아서 배열에 넣기
+    if (groupMembers.length > 0) {
+      const randomMembers = GetRandomItems(groupMembers, 3);
+      const updatedMembers = randomMembers.map((member) => (
+        <MemberListSidebar
+          userId={member._id}
+          src={member.profileImage[0]} // 나중에 이미지 URL로 교체
+          nickname={member.nickName}
+          introduction={member.introduce}
+        />
+      ));
+      setMemberArray(updatedMembers);
+    }
   }, []);
+
+  // const imageSrc = `${UPLOADED_IMG_URL}${post?.postImage}`; // 교체시 참고
 
   // 피드 글 조회 API
   useEffect(() => {
@@ -91,8 +115,11 @@ const Community: React.FC = () => {
           }
         });
 
+        // 게시글 최신순 정렬
+        const sortedPosts = sortByLatest(filteredDeletedPosts);
+
         // 그룹 id가 같은 포스트만 가지고 오기
-        const filteredGroupPosts = filteredDeletedPosts.filter(
+        const filteredGroupPosts = sortedPosts.filter(
           (post: Post) => post.categoryId._id === groupId,
         );
         setPosts(filteredGroupPosts);
@@ -128,19 +155,22 @@ const Community: React.FC = () => {
     fetchData();
   };
 
-  const handleSearch = (searchValue: string) => {
-    console.log('검색어:', searchValue);
-    if (searchValue.trim() === '') {
-      // 검색어가 없는 경우 전체 글을 보여줌(띄어쓰기만 있는 경우 포함)
+  const handleSearch = (value: string) => {
+    const trimmedValue = value.trim(); // 공백 제거
+    setSearchValue(trimmedValue);
+    console.log('검색어:', trimmedValue); // 추후 삭제
+
+    if (trimmedValue === '') {
+      // 검색어가 없는 경우 전체 글을 보여줌
       setFilteredSearchPosts(posts);
       setSearchedPostCount(posts.length);
     } else {
       // 검색어 입력 시 검색어를 포함한 피드 글 필터링해서 보여줌
       const filteredPosts = posts.filter((post) =>
-        post.title.includes(searchValue),
+        post.title.includes(trimmedValue),
       );
       setFilteredSearchPosts(filteredPosts);
-      setSearchedPostCount(filteredPosts.length); 
+      setSearchedPostCount(filteredPosts.length);
     }
   };
 
@@ -163,6 +193,17 @@ const Community: React.FC = () => {
           </WritingButton>
           {searchedPostCount > 0 ? (
             filteredSearchPosts.map((post) => (
+              <FeedBox
+                key={post._id}
+                title={post.title}
+                content={post.content}
+                src={profileImg}
+                nickname={post?.userId?.nickName}
+                uploadedDate={post.createdAt}
+              />
+            ))
+          ) : searchValue === null ? (
+            posts.map((post) => (
               <FeedBox
                 key={post._id}
                 title={post.title}
@@ -217,14 +258,14 @@ const SidePanelContainer = styled.div`
 `;
 
 const P = styled.p`
-width: 100%;
-display: flex;
-justify-content: center;
-font-size: var(--font-size-hd-1);
-padding: 80px 0;
-color: var(--color-grey-1);
-font-weight: var(--font-weight-regular);
-`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  font-size: var(--font-size-hd-1);
+  padding: 80px 0;
+  color: var(--color-grey-1);
+  font-weight: var(--font-weight-regular);
+`;
 
 const GroupOption = styled.div`
   display: flex;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import { API_URL } from '../../constants/constants';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { userState } from '../../recoil/atoms';
 
 // 컴포넌트
@@ -17,7 +17,203 @@ import WritingModalButton from '../../components/community/WritingModalButton';
 import CategoryOptions from '../../constants/CategoryOptions'; //select 대분류
 
 // 로직
-import GetRandomItems from '../../utils/GetRandomThreeItems';
+import GetRandomItems from '../../utils/GetRandomThreeItems'; // 랜덤으로 그룹 뽑기
+import sortByLatest from '../../utils/sortByLatest'; // 최신순 정렬
+
+interface Posts {
+  _id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  userId: {
+    profileImage: string[];
+    nickName: string;
+  };
+}
+
+interface Groups {
+  _id: string;
+  group: string;
+  introduction: string;
+}
+
+const Home: React.FC = () => {
+  const [user] = useRecoilState(userState);
+
+  const [posts, setPosts] = useState<Posts[]>([]); // 필터링될 게시글
+  const [totalPost, setTotalPost] = useState<Posts[]>([]); // 전체 게시글
+  const [groups, setGroups] = useState<Groups[]>([]);
+  const [groupArray, setGroupArray] = useState<JSX.Element[]>([]); // 그룹 추천용 그룹배열
+
+  const [selectedCategoryValue, setSelectedCategotyValue] = useState(''); // 대분류
+  const [selectedGroupCategoryId, setselectedGroupCategoryId] = useState(''); // 소분류(그룹의 id)
+  const [selectedGroupOptions, setSelectedGroupOptions] = useState<any[]>([]); // 소분류(select용 배열), 타입 추후 수정
+
+  // 그룹 불러오기
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${API_URL}groups`);
+        setGroups(response.data.message);
+        console.log('그룹 조회 성공');
+      } catch (error) {
+        console.error('그룹 조회 실패', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // 그룹 관련 로직
+  useEffect(() => {
+    // 추천 그룹 뽑아서 배열에 넣기
+    if (groups.length > 0) {
+      const randomGroups = GetRandomItems(groups, 3);
+      const updatedGroups = randomGroups.map((group) => (
+        <CommunityListSidebar
+          key={group?._id}
+          groupId={group?._id}
+          name={group?.group}
+          introduction={group?.introduction}
+        />
+      ));
+      setGroupArray(updatedGroups);
+    }
+  }, [groups]);
+
+  interface Post {
+    deletedAt: string | null;
+  }
+  // 피드 글 불러오기
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${API_URL}post`);
+        //deletedAt이 null이 아닌 게시물만 필터링
+        const filteredDeletedPosts: Post[] = [];
+        response.data.message.forEach((post: Post) => {
+          if (post.deletedAt === null) {
+            filteredDeletedPosts.push(post);
+          }
+        });
+
+        // 게시글 최신순 정렬
+        const sortedPosts = sortByLatest(filteredDeletedPosts)
+        setPosts(sortedPosts);
+
+        // 모든 게시글
+        setTotalPost(filteredDeletedPosts);
+
+        // 그룹 아이디가 현재 select된 요소와 같은 것만 가지고 오기
+        const filteredGroupPosts: Post[] = [];
+        filteredDeletedPosts.forEach((post: Post) => {
+          // console.log('포스트.카테고리아이디._id)', post.categoryId._id);
+          if (post.categoryId._id === selectedGroupCategoryId) {
+            filteredGroupPosts.push(post);
+          }
+        });
+
+        setPosts(filteredGroupPosts);
+        console.log('피드 글 조회 성공');
+      } catch (error) {
+        console.error('피드 글 조회 실패', error);
+      }
+    };
+    fetchData();
+  }, [selectedCategoryValue, selectedGroupCategoryId]);
+
+  // select -> 현재 선택된 대분류를 받고, 그에 해당되는 그룹을 보여주는 형식
+  const handleCategoryChange = (selectedOption: {
+    value: string;
+    label: string;
+  }) => {
+    setSelectedCategotyValue(selectedOption.value); // 현재 선택된 대분류를 상태에 업데이트(string값, 0 혹은 1)
+
+    // 선택된 카테고리에 해당하는 그룹만 필터링하여 업데이트하고 받아오기
+    const filteredGroupsOptions = user?.categoryId
+      .filter(
+        (category) =>
+          category.name.toString() === selectedOption.value.toString(),
+      )
+      .map((category) => ({
+        value: category._id,
+        label: category.group,
+      }));
+
+    filteredGroupsOptions.unshift({
+      // 대분류를 바꾸면 새로 그룹 선택하도록 동작
+      value: 'select',
+      label: '그룹 선택',
+    });
+
+    setSelectedGroupOptions(filteredGroupsOptions);
+  };
+
+  const handleSelectedGroup = (selectedOption: {
+    value: string;
+    label: string;
+  }) => {
+    setselectedGroupCategoryId(selectedOption.value); // 현재 선택된 소분류를 상태에 업데이트
+  };
+
+  return (
+    <>
+      <Banner />
+      <ContentContainer>
+        <FeedContainer>
+          <FeedOption>
+            <Classification>
+              <p>분류:</p>
+              <CommunitySelect
+                width="100px"
+                borderRadius="30px"
+                options={CategoryOptions}
+                onChange={handleCategoryChange}
+              />
+              <CommunitySelect
+                width="120px"
+                borderRadius="30px"
+                options={selectedGroupOptions}
+                onChange={handleSelectedGroup}
+              />
+            </Classification>
+            <WritingButton>
+              <p>함께 나누고 싶은 이야기가 있나요?</p>
+              <WritingModalButton />
+            </WritingButton>
+          </FeedOption>
+          {selectedCategoryValue === ''
+            ? totalPost.map((post) => (
+                <FeedBox
+                  postId={post?._id}
+                  key={post?._id}
+                  title={post?.title}
+                  content={post?.content}
+                  profile={post?.userId?.profileImage[0]}
+                  nickname={post?.userId?.nickName}
+                  uploadedDate={post?.createdAt}
+                />
+              ))
+            : posts.map((post) => (
+                <FeedBox
+                  postId={post?._id}
+                  key={post?._id}
+                  title={post?.title}
+                  content={post?.content}
+                  profile={post?.userId?.profileImage[0]}
+                  nickname={post?.userId?.nickName}
+                  uploadedDate={post?.createdAt}
+                />
+              ))}
+        </FeedContainer>
+        <SidePanelContainer>
+          <SidePanel name="추천 커뮤니티" array={groupArray} />
+        </SidePanelContainer>
+      </ContentContainer>
+    </>
+  );
+};
+
+export default Home;
 
 const ContentContainer = styled.div`
   margin-top: 80px;
@@ -66,180 +262,3 @@ const WritingButton = styled.div`
     padding: 0 10px;
   }
 `;
-
-interface Posts {
-  _id: string;
-  title: string;
-  content: string;
-  createdAt: string;
-  userId: {
-    profileImage: string[];
-    nickName: string;
-  };
-}
-
-interface Groups {
-  _id: string;
-  group: string;
-  introduction: string;
-}
-
-const Home: React.FC = () => {
-  const [user] = useRecoilState(userState);
-
-  const [posts, setPosts] = useState<Posts[]>([]);
-  const [groups, setGroups] = useState<Groups[]>([]);
-  const [groupArray, setGroupArray] = useState<JSX.Element[]>([]); // 그룹 추천용 그룹배열
-
-  const [selectedCategoryValue, setSelectedCategotyValue] = useState(''); // 대분류
-  const [selectedGroupCategoryId, setselectedGroupCategoryId] = useState(''); // 소분류(그룹의 id)
-  const [selectedGroupOptions, setSelectedGroupOptions] = useState<any[]>([]); // 소분류(select용 배열), 타입 추후 수정
-
-  
-  // 그룹 불러오기
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`${API_URL}groups`);
-        setGroups(response.data.message);
-        console.log('그룹 조회 성공');
-      } catch (error) {
-        console.error('그룹 조회 실패', error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  // 그룹 관련 로직
-  useEffect(() => {
-    // 추천 그룹 뽑아서 배열에 넣기
-    if (groups.length > 0) {
-      const randomGroups = GetRandomItems(groups, 3);
-      const updatedGroups = randomGroups.map((group, index) => (
-        <CommunityListSidebar
-          key={index}
-          groupId={group?._id}
-          name={group?.group}
-          introduction={group?.introduction}
-        />
-      ));
-      setGroupArray(updatedGroups);
-    }
-  }, [groups]);
-
-  interface Post {
-    deletedAt: string | null;
-  }
-  // 피드 글 불러오기
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`${API_URL}post`);
-        //deletedAt이 null이 아닌 게시물만 필터링
-        const filteredDeletedPosts: Post[] = [];
-        response.data.message.forEach((post: Post) => {
-          if (post.deletedAt === null) {
-            filteredDeletedPosts.push(post);
-          }
-        });
-
-        // 그룹 아이디가 현재 select된 요소와 같은 것만 가지고 오기
-        const filteredGroupPosts: Post[] = [];
-        filteredDeletedPosts.forEach((post: Post) => {
-          // console.log('포스트.카테고리아이디._id)', post.categoryId._id);
-          if (post.categoryId._id === selectedGroupCategoryId) {
-            filteredGroupPosts.push(post);
-          }
-        });
-
-        setPosts(filteredGroupPosts);
-        console.log('피드 글 조회 성공');
-      } catch (error) {
-        console.error('피드 글 조회 실패', error);
-      }
-    };
-    fetchData();
-  }, [selectedCategoryValue, selectedGroupCategoryId]);
-
-  // select -> 현재 선택된 대분류를 받고, 그에 해당되는 그룹을 보여주는 형식
-  const handleCategoryChange = (selectedOption: {
-    value: string;
-    label: string;
-  }) => {
-    setSelectedCategotyValue(selectedOption.value); // 현재 선택된 대분류를 상태에 업데이트(string값, 0 혹은 1)
-    // console.log(selectedOption.value, selectedOption.label); // 디버깅용 - 현재 선택된 대분류 찍어보는 콘솔 -> 추후 삭제
-
-    // 선택된 카테고리에 해당하는 그룹만 필터링하여 업데이트하고 받아오기
-    const filteredGroupsOptions = user?.categoryId
-      .filter(
-        (category) =>
-          category.name.toString() === selectedOption.value.toString(),
-      )
-      .map((category) => ({
-        value: category._id,
-        label: category.group,
-      }));
-
-    filteredGroupsOptions.unshift({ //대분류를 바꾸면 그룹 선택하도록 동작
-      value: 'select',
-      label: '그룹 선택',
-    });
-
-    setSelectedGroupOptions(filteredGroupsOptions);
-  };
-
-  const handleSelectedGroup = (selectedOption: {
-    value: string;
-    label: string;
-  }) => {
-    setselectedGroupCategoryId(selectedOption.value); // 현재 선택된 소분류를 상태에 업데이트
-  };
-
-
-  return (
-    <>
-      <Banner />
-      <ContentContainer>
-        <FeedContainer>
-          <FeedOption>
-            <Classification>
-              <p>분류:</p>
-              <CommunitySelect
-                width="100px"
-                borderRadius="30px"
-                options={CategoryOptions}
-                onChange={handleCategoryChange}
-              />
-              <CommunitySelect
-                width="120px"
-                borderRadius="30px"
-                options={selectedGroupOptions}
-                onChange={handleSelectedGroup}
-              />
-            </Classification>
-            <WritingButton>
-              <p>함께 나누고 싶은 이야기가 있나요?</p>
-              <WritingModalButton />
-            </WritingButton>
-          </FeedOption>
-          {posts.map((post, index) => (
-            <FeedBox
-              postId={post?._id}
-              key={index}
-              title={post?.title}
-              content={post?.content}
-              profile={post?.userId?.profileImage[0]}
-              nickname={post?.userId?.nickName}
-              uploadedDate={post?.createdAt}
-            />
-          ))}
-        </FeedContainer>
-        <SidePanelContainer>
-          <SidePanel name="추천 커뮤니티" array={groupArray} />
-        </SidePanelContainer>
-      </ContentContainer>
-    </>
-  );
-};
-
-export default Home;
