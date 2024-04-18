140cfe10a10f9d6a0566f19a8316f7942521ef08
petitesize <petitesize22@gmail.com>
2024-04-18 19:12:45 +0900

    API_URL

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 // 로컬테스트는 아래 주석 변경하고 사용
- export const API_URL = 'http://localhost:3001/api/';
+// export const API_URL = 'http://localhost:3001/api/';
 // dev에 올릴 때 절대 수정 금지
-// export const API_URL = 'http://kdt-sw-8-team01.elicecoding.com/api/';
-=======
-// 상수 파일
-export const API_URL = 'http://localhost:3001/api/';
-//export const API_URL = 'http://kdt-sw-8-team01.elicecoding.com/api/';
-
->>>>>>> 890f83ccae5baf0d5e56847b441801a3987abb30
+export const API_URL = 'http://kdt-sw-8-team01.elicecoding.com/api/';
 export const DOMAIN_URL = 'http://kdt-sw-8-team01.elicecoding.com/';
 
 // post 이미지 불러오는 URL
