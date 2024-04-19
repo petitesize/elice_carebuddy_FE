3d59b5786707835b48dd88f934eef8d249307872
petitesize <petitesize22@gmail.com>
2024-04-20 03:56:15 +0900

    Fix: test9

--- src/pages/signupInfo/SignupInfo.tsx
+++ src/pages/signupInfo/SignupInfo.tsx
@@ -5,19 +5,19 @@ import { userToken } from '../../recoil/selectors';
 
 const SignupInfo: React.FC = () => {
   // 여기서 토큰 있으면 my api 호출해서 유저 정보 받아오기
-  const setUser = useSetRecoilState(userToken);
+  // const setUser = useSetRecoilState(userToken);
 
-  useEffect(() => {
-    //check if "access_token" exists in Cookie and set it to Recoil
-    const token = document.cookie
-      .split(';')
-      .find((cookie) => cookie.trim().startsWith('accessToken='))
-      ?.split('=')[1];
+  // useEffect(() => {
+  //   //check if "access_token" exists in Cookie and set it to Recoil
+  //   const token = document.cookie
+  //     .split(';')
+  //     .find((cookie) => cookie.trim().startsWith('accessToken='))
+  //     ?.split('=')[1];
 
-    if (token) {
-      setUser(token);
-    }
-  }, []);
+  //   if (token) {
+  //     setUser(token);
+  //   }
+  // }, []);
 
   return (
     <>
